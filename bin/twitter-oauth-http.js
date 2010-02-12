43439062b0025eb6c8795f586f262476e6d1909d
David Rice <me@davidjrice.co.uk>
2010-02-12 04:11:49 +0000

    Improve signature field preparation

--- bin/twitter-oauth-http.js
+++ bin/twitter-oauth-http.js
@@ -3,8 +3,9 @@ require('../lib/bootstrap');
 var http = require("http"),
     crypto = require('dep/node-crypto').crypto,
     querystring = require("querystring"),
-    twitter = http.createClient(80, "twitter.com");
-
+    twitter = http.createClient(61213, "localhost");
+//  twitter = http.createClient(80, "twitter.com");
+    
 var request_headers = {
   "Authorization": {
     "realm": "",
@@ -21,12 +22,13 @@ var token_secret = null;
 var consumer_secret = "0q3N1wUJKjXeM5R84YhaymsEAFpPVbUoBEOwS3ThuAo"
 var signature_key = consumer_secret + "&" + token_secret
 
-var signature_base_string = escape("POST&http://twitter.com/oauth/request_token&" + querystring.stringify(request_headers.Authorization));
-var signature = escape(crypto.b64_hmac_sha1(signature_base_string, signature_key));
+var signature_base_string = "GET&"+encodeURIComponent("http://twitter.com/oauth/request_token")+"&"+encodeURIComponent(querystring.stringify(request_headers.Authorization));
+debug("SIGNATURE_BASE: " + signature_base_string);
+var signature = encodeURIComponent(crypto.b64_hmac_sha1(signature_base_string, signature_key));
 request_headers.Authorization.oauth_signature = signature
 
 
-function prepareHeader(params){
+function prepare_header(params){
   var stringified = 'OAuth '
   for(var key in params){
     var value = params[key]
@@ -39,9 +41,9 @@ function prepareHeader(params){
   return stringified
 }
 
-var request = twitter.request("POST", "/oauth/request_token", {
+var request = twitter.request("GET", "/oauth/request_token", {
   "Host": "twitter.com",
-  "Authorization": prepareHeader(request_headers.Authorization)
+  "Authorization": prepare_header(request_headers.Authorization)
 });
 
 debug("REQUEST:");
